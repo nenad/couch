141ce981ab67941b599e642b20fa7548a01ff535
Nenad Stojanovikj <me+github@nenad.dev>
2019-05-01 14:07:27 +0200

    Make CLI commands interruptable

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,23 @@ import (
 	"database/sql"
 	"github.com/nenadstojanovikj/couch/pkg/config"
 	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func NewCLI(conf *config.Config, db *sql.DB) *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use: "couch",
 	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sig
+		os.Exit(1)
+	}()
 
 	rootCmd.AddCommand(NewAppCommand(conf, db))
 	rootCmd.AddCommand(NewAuthCommand(conf))
