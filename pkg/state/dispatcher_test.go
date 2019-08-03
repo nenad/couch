25cbcad984bddbf61e2a5add2b70dcded88b12f8
Nenad Stojanovikj <me+github@nenad.dev>
2019-08-03 13:14:13 +0200

    Add error test cases for the dispatcher

--- pkg/state/dispatcher_test.go
+++ pkg/state/dispatcher_test.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/nenad/couch/pkg/media"
@@ -34,6 +35,29 @@ func TestDispatcher_Scrape(t *testing.T) {
 	for _, m := range expMagnets {
 		assert.Contains(t, res.Value, m)
 	}
+}
+
+func TestDispatcher_ScrapeError(t *testing.T) {
+	d := NewDispatcher()
+	expItem := items[0]
+	expMagnets := []storage.Magnet{
+		{Item: expItem, Location: "magnet://1"},
+		{Item: expItem, Location: "magnet://2"},
+	}
+
+	d.OnScrape(func(item media.SearchItem) ScrapeResult {
+		return ScrapeResult{Value: []storage.Magnet{expMagnets[0]}}
+	})
+	d.OnScrape(func(item media.SearchItem) ScrapeResult {
+		return ScrapeResult{nil, fmt.Errorf("something bad happened")}
+	})
+
+	res := d.Scrape(expItem)
+
+	assert.Equal(t, ScrapeResult{
+		Value: nil,
+		Error: fmt.Errorf("something bad happened"),
+	}, res)
 }
 
 func TestDispatcher_Extract(t *testing.T) {
@@ -80,6 +104,27 @@ func TestDispatcher_Extract(t *testing.T) {
 	}
 }
 
+func TestDispatcher_ExtractError(t *testing.T) {
+	d := NewDispatcher()
+
+	// Episode extractor
+	d.OnExtract(func(magnets []storage.Magnet) ExtractResult {
+		return ExtractResult{Value: []storage.Download{}}
+	})
+
+	// Movie extractor
+	d.OnExtract(func(magnets []storage.Magnet) ExtractResult {
+		return ExtractResult{Error: fmt.Errorf("extraction failed")}
+	})
+
+	res := d.Extract([]storage.Magnet{
+		{Item: items[0]},
+		{Item: items[1]},
+	})
+
+	assert.Equal(t, ExtractResult{Value:nil, Error:fmt.Errorf("extraction failed")}, res)
+}
+
 func TestDispatcher_Download(t *testing.T) {
 	d := NewDispatcher()
 
@@ -117,4 +162,25 @@ func TestDispatcher_Download(t *testing.T) {
 	for _, i := range items {
 		assert.Contains(t, res.Value, i)
 	}
+}
+
+func TestDispatcher_DownloadError(t *testing.T) {
+	d := NewDispatcher()
+
+	// Episode downloader
+	d.OnDownload(func(downloads []storage.Download) DownloadResult {
+		return DownloadResult{Value: items}
+	})
+
+	// Movie downloader
+	d.OnDownload(func(downloads []storage.Download) DownloadResult {
+		return DownloadResult{Error: fmt.Errorf("download error")}
+	})
+
+	res := d.Download([]storage.Download{
+		{Item: items[0]},
+		{Item: items[1]},
+	})
+
+	assert.Equal(t, DownloadResult{Value:nil, Error:fmt.Errorf("download error")}, res)
 }
